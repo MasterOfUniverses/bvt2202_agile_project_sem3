11de05c710956d34a2706f1ecc2fe95fe0711c5a
HypnoFroggy <125369033+HypnoFroggy@users.noreply.github.com>
2023-11-03 16:03:00 +0300

    Update init_db.bat

--- source/db/init_db.bat
+++ source/db/init_db.bat
@@ -1,4 +1,4 @@
-PGPASSWORD= admin_lab78
+SEt PGPASSWORD=admin_lab78
 psql -U postgres -d postgres -h localhost -p 5432 -a -f "db_start_script.sql"
 psql -U postgres -d bot_timetable -h localhost -p 5432 -a -f "create_script.sql"
 psql -U postgres -d bot_timetable -h localhost -p 5432 -a -f "insert_script.sql"
