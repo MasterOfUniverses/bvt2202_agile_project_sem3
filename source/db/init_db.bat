7a0c60b730a9675c5324b45933416cb2eba8e8a5
HypnoFroggy <125369033+HypnoFroggy@users.noreply.github.com>
2023-11-03 16:01:12 +0300

    проба бат файла

--- source/db/init_db.bat
+++ source/db/init_db.bat
@@ -1,4 +1,5 @@
-psql -U postgres -W admin_lab78 -d postgres -h localhost -p 5432 -a -f "db_start_script.sql"
-psql -U postgres -W admin_lab78 -d bot_timetable -h localhost -p 5432 -a -f "create_script.sql"
-psql -U postgres -W admin_lab78 -d bot_timetable -h localhost -p 5432 -a -f "insert_script.sql"
-psql -U postgres -W admin_lab78 -d bot_timetable -h localhost -p 5432 -a -f "insert_script2.sql"
+PGPASSWORD= admin_lab78
+psql -U postgres -d postgres -h localhost -p 5432 -a -f "db_start_script.sql"
+psql -U postgres -d bot_timetable -h localhost -p 5432 -a -f "create_script.sql"
+psql -U postgres -d bot_timetable -h localhost -p 5432 -a -f "insert_script.sql"
+psql -U postgres -d bot_timetable -h localhost -p 5432 -a -f "insert_script2.sql"
