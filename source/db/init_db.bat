b9551e84f9c5964b8ff8be852eb22602aec9c561
HypnoFroggy <125369033+HypnoFroggy@users.noreply.github.com>
2023-11-03 15:57:34 +0300

    проба новой init_db.bat

--- source/db/init_db.bat
+++ source/db/init_db.bat
@@ -1,4 +1,4 @@
-psql -U postgres -d postgres -h localhost -p 5432 -a -f "db_start_script.sql"
-psql -U postgres -d bot_timetable -h localhost -p 5432 -a -f "create_script.sql"
-psql -U postgres -d bot_timetable -h localhost -p 5432 -a -f "insert_script.sql"
-psql -U postgres -d bot_timetable -h localhost -p 5432 -a -f "insert_script2.sql"
+psql -U postgres -W admin_lab78 -d postgres -h localhost -p 5432 -a -f "db_start_script.sql"
+psql -U postgres -W admin_lab78 -d bot_timetable -h localhost -p 5432 -a -f "create_script.sql"
+psql -U postgres -W admin_lab78 -d bot_timetable -h localhost -p 5432 -a -f "insert_script.sql"
+psql -U postgres -W admin_lab78 -d bot_timetable -h localhost -p 5432 -a -f "insert_script2.sql"
